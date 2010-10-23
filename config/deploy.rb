3f0655ff72d56c74ec201cc47cefc67fad7a90fe
Alex Rozumiy <brain-geek@yandex.ua>
2010-10-23 16:40:18 +0300

    Fixing deploy scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,18 +33,7 @@ role :db,  domain, :primary => true
 #############################################################
 
 #update bundle
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
- 
-  task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
-    run "cd #{release_path} && bundle install --without test development"
-  end
-end
+require 'bundler/capistrano'
  
 after 'deploy:update_code', 'bundler:bundle_new_release'
 
