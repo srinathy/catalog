aa6f0815ae7a7b2b8ed57d161eb93535ea3cfac9
Alex Rozumiy <brain-geek@yandex.ua>
2011-01-03 02:26:25 +0200

    Minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  add_crumb I18n.t("index_page"), '/'
+  before_filter :init_page
 
   private
+  def init_page
+    add_crumb I18n.t("index_page"), '/'
+  end
+  
   def add_category_crumbs(obj)
     unless (obj.is_a? Category)
       obj = Category.find_by_id(obj)
